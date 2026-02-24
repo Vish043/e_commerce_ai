4dab651c93e46e7e4323b5a1735d4e82d68820b8
Vish043 <vishalgaonkar2004@gmail.com>
2026-02-24 21:51:37 +0530

    changes in server,js

--- server.js
+++ server.js
@@ -17,8 +17,12 @@ mongoose.connect(process.env.MONGO_URI)
 
 app.use("/webhook", webhookRoute);
 
+app.get("/", (req, res) => {
+  res.send("🚀 Backend is running and ready to handle requests!");
+});
+
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
